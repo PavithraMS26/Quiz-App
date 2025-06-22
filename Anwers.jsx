e6950252d22462afea5c05831e166dfb14d4607d
Pavithra.MS <25pavithra.ms@gmail.com>
2025-06-22 21:17:20 +0530

    Add files via upload

---
+++ Anwers.jsx
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+
+export default function Answers({ answers, selectedAnswer, answerState, onSelect }) {
+    const shuffledAnswer = useRef();
+
+    if (!shuffledAnswer.current) {
+        shuffledAnswer.current = [...answers];
+        shuffledAnswer.current.sort(() => Math.random() - 0.5);
+    }
+
+    return (
+        <ul id="answers">
+            {shuffledAnswer.current.map((answer, index) => {
+                const isSelected = selectedAnswer === answer;
+
+                let cssClasses = '';
+                if (answerState === '' && isSelected) {
+                    cssClasses = 'selected';
+                }
+                if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                    cssClasses = answerState;
+                }
+
+                return (
+                    <li key={`${answer}-${index}`} className="answer">
+                        <button
+                            onClick={() => onSelect(answer)}
+                            className={cssClasses}
+                        >
+                            {answer}
+                        </button>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
