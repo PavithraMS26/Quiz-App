cace19706a14f20de1fed8e24ae143b3d983af83
Pavithra.MS <25pavithra.ms@gmail.com>
2025-06-22 21:12:48 +0530

    Update Quiz.jsx

--- Quiz.jsx
+++ Quiz.jsx
@@ -1,9 +1,10 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from '../questions.js';
 import quizCompImg from '../assets/quiz-complete.png';
-import Timer from "./Timer.jsx";
+import Question from './Question.jsx'
 
 export default function Quiz() {
+
     const [answerState, setAnswerState] = useState('');
     const [userAnswer, setUserAnswer] = useState([]);
 
@@ -39,44 +40,10 @@ export default function Quiz() {
         );
     }
 
-    const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswer.sort(() => Math.random() - 0.5);
-
     return (
         <div id="quiz">
-            <div id="question">
-                <Timer
-                    key={activeQuestionIndex}
-                    timeout={10000}
-                    onTimeOut={handleSkipAns}
-                />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswer.map((answer) => {
-                        const lastAnswer = userAnswer[userAnswer.length - 1];
-                        const isSelected = lastAnswer === answer;
-
-                        let cssClasses = '';
-                        if (answerState === '' && isSelected) {
-                            cssClasses = 'selected';
-                        }
-                        if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
-                            cssClasses = answerState;
-                        }
-
-                        return (
-                            <li key={answer} className="answer">
-                                <button
-                                    onClick={() => handleSelectAnswer(answer)}
-                                    className={cssClasses}
-                                >
-                                    {answer}
-                                </button>
-                            </li>
-                        );
-                    })}
-                </ul>
-
+            
+                
                 <div id="skip-action">
                     <button onClick={handleSkipAns}>Skip</button>
                 </div>
